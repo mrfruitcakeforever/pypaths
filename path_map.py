b6b26f534058dc3a571c9fea09ff18bca122f663
angsty watermelon <angsty watermelon>
2023-02-01 11:09:20 -0500

    added multi_function_hash_map

--- path_map.py
+++ path_map.py
@@ -1,6 +1,7 @@
 from jsonpath_ng import parse
 from functools import partial
 from pathlib import Path
+from toolz import identity, itemmap
 
 def jsonpath_values(source:dict, path:str):
     parser = parse(path)
@@ -12,4 +13,15 @@ def jsonpath_values(source:dict, path:str):
 def path_from_dict(path: Path, source:dict):
     json_path = '.'.join(path.parts)
     return jsonpath_values(source, json_path)
+
+def as_callable(element):
+    return partial(identity, element)
+
+def multi_function_hash_map(source: dict):
+    ''' key function is source and value is parser'''
+    return [item(key()) for key,item in source.items()]
+
+# {key: functional_value}
+# {f1: f2{
+
 
