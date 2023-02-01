e2737bf374b6fcb7028cc2254fcd9c6df5af8a95
angsty watermelon <angsty watermelon>
2023-02-01 10:19:16 -0500

    added path from dict

--- path_map.py
+++ path_map.py
@@ -1,5 +1,6 @@
 from jsonpath_ng import parse
 from functools import partial
+from pathlib import Path
 
 def jsonpath_values(source:dict, path:str):
     parser = parse(path)
@@ -7,4 +8,8 @@ def jsonpath_values(source:dict, path:str):
     if matches:
         return list(map(lambda match:match.value, matches))
     return matches
+
+def path_from_dict(path: Path, source:dict):
+    json_path = '.'.join(path.parts)
+    return jsonpath_values(source, json_path)
 
